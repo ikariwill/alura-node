9d8e677674af430601d3b5e0efd323fba3abcdb6
Willian Home <willian@wiki4fit.com.br>
2019-01-19 17:28:27 -0200

    aula 1

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+const server = http.createServer(function (req, res){
+    let html;
+    if(req.url == '/') {
+        html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1>Casa do código</h1>
+                </body>
+            </html>
+        `;
+    } else if(req.url == '/livros') {
+        html = `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Livros</h1>
+            </body>
+        </html>
+    `;
+    }
+    res.end(html);
+});
+server.listen(3000);
